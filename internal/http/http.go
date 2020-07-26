924e032db5f6af2845a9963b726643984962e8fd
d.zyuzin <d.zyuzin@spacemir.ru>
2020-07-26 18:27:06 +0300

    add routing for slots and banners

--- internal/http/http.go
+++ internal/http/http.go
@@ -10,14 +10,23 @@ func NewHandler(store *storage.Storage) (http.Handler, error) {
 	router := mux.NewRouter()
 
 	groupsHandler := NewSDGroupHandler(store.SDGroups)
+	bannersHandler := NewBannerHandler(store.Banners)
+	slotsHandler := NewSlotHandler(store.Slots)
 
 	groups := router.PathPrefix("/sd-groups").Subrouter()
-	groups.Handle("", asHandler(groupsHandler.Create)).Methods("POST")
-	groups.Handle("", asHandler(groupsHandler.GetAll)).Methods("GET")
+	groups.Handle("", http.HandlerFunc(groupsHandler.Create)).Methods("POST")
+	groups.Handle("", http.HandlerFunc(groupsHandler.GetAll)).Methods("GET")
+	groups.Handle("", http.HandlerFunc(groupsHandler.Delete)).Methods("DELETE")
+
+	banners := router.PathPrefix("/banners").Subrouter()
+	banners.Handle("", http.HandlerFunc(bannersHandler.Create)).Methods("POST")
+	banners.Handle("", http.HandlerFunc(bannersHandler.GetAll)).Methods("GET")
+	banners.Handle("", http.HandlerFunc(bannersHandler.Delete)).Methods("DELETE")
+
+	slots := router.PathPrefix("/slots").Subrouter()
+	slots.Handle("", http.HandlerFunc(slotsHandler.Create)).Methods("POST")
+	slots.Handle("", http.HandlerFunc(slotsHandler.GetAll)).Methods("GET")
+	slots.Handle("", http.HandlerFunc(slotsHandler.Delete)).Methods("DELETE")
 
 	return router, nil
-}
-
-func asHandler(in func(w http.ResponseWriter, r *http.Request)) http.HandlerFunc {
-	return in
 }
--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,7 +1,13 @@
 package storage
 
-import "github.com/dbzyuzin/banners-rotation.git/internal/sdgroup"
+import (
+	"github.com/dbzyuzin/banners-rotation.git/internal/banner"
+	"github.com/dbzyuzin/banners-rotation.git/internal/sdgroup"
+	"github.com/dbzyuzin/banners-rotation.git/internal/slot"
+)
 
 type Storage struct {
 	SDGroups sdgroup.Storage
+	Banners  banner.Storage
+	Slots    slot.Storage
 }
